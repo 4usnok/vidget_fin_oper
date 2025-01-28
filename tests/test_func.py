b93cb4ec4ee5282c3cfaa0a153c5f1e41258e2d6
Nikolay Korkushko <Kakashonochka12@gmail.com>
2025-01-28 03:14:17 +0300

    Homework 10.2 finally

--- tests/test_func.py
+++ tests/test_func.py
@@ -4,33 +4,35 @@ from src.widget import get_date, mask_account_card
 import pytest
 
 
-@pytest.mark.parametrize(
-    "number_card, number_card_2, number_card_3", [
-        ("7000792289606361",
-         "73654108430222135874",
-         "1596837868705199"
-         )]
-)
+@pytest.mark.parametrize("number_card", ["7000792289606361"])
 def test_get_mask_card_number(number_card):
     assert get_mask_card_number("7000792289606361")
 
 
+@pytest.mark.parametrize("number_card_2", ["73654108430222135874"])
 def test_get_mask_account(number_card_2):
     assert get_mask_account("73654108430222135874")
 
 
+@pytest.mark.parametrize("number_card_3", ["1596837868705199"])
 def test_mask_account_card(number_card_3):
     assert mask_account_card("1596837868705199")
 
 
-@pytest.mark.parametrize("set_of_dict, state", [
-        ([
-            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-        ], "CANCELED")
-])
+@pytest.mark.parametrize(
+    "set_of_dict, state",
+    [
+        (
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ],
+            "CANCELED",
+        )
+    ],
+)
 def test_filter_by_state(set_of_dict, state):
     assert filter_by_state(
         [
