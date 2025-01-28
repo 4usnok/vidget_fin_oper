4267c919617713b33c310b02270db807681dc0d2
Nikolay Korkushko <Kakashonochka12@gmail.com>
2025-01-28 03:01:26 +0300

    Homework 10.2 parametrize

--- tests/test_func.py
+++ tests/test_func.py
@@ -1,17 +1,37 @@
 from src.masks import get_mask_account, get_mask_card_number
 from src.processing import filter_by_state, sort_by_date
 from src.widget import get_date, mask_account_card
+import pytest
 
 
-def test_get_mask_card_number():
+@pytest.mark.parametrize(
+    "number_card, number_card_2, number_card_3", [
+        ("7000792289606361",
+         "73654108430222135874",
+         "1596837868705199"
+         )]
+)
+def test_get_mask_card_number(number_card):
     assert get_mask_card_number("7000792289606361")
 
 
-def test_get_mask_account():
+def test_get_mask_account(number_card_2):
     assert get_mask_account("73654108430222135874")
 
 
-def test_filter_by_state():
+def test_mask_account_card(number_card_3):
+    assert mask_account_card("1596837868705199")
+
+
+@pytest.mark.parametrize("set_of_dict, state", [
+        ([
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ], "CANCELED")
+])
+def test_filter_by_state(set_of_dict, state):
     assert filter_by_state(
         [
             {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
@@ -19,10 +39,20 @@ def test_filter_by_state():
             {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
             {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
         ],
-        "CANCELED",)
+        "CANCELED",
+    )
 
 
-def test_sort_by_date():
+@pytest.mark.parametrize(
+    "list_dict",
+    [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ],
+)
+def test_sort_by_date(list_dict):
     assert sort_by_date(
         [
             {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
@@ -33,8 +63,6 @@ def test_sort_by_date():
     )
 
 
-def test_get_data():
+@pytest.mark.parametrize("data_num", ["2024-03-11T02:26:18.671407"])
+def test_get_data(data_num):
     assert get_date("2024-03-11T02:26:18.671407")
-
-def test_mask_account_card():
-    assert mask_account_card("1596837868705199")
