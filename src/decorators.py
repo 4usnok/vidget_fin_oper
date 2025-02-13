d5cec8d2da2b3e8b4ac17303fbe43d7293080cd8
Nikolay Korkushko <Kakashonochka12@gmail.com>
2025-02-13 23:15:11 +0300

    Homework 11.2 test.py

--- src/decorators.py
+++ src/decorators.py
@@ -1,30 +1,65 @@
+import pytest
+
+
 def log(filename):
     """
     Декоратор, который принимает параметр filename
     """
+
     def my_decorator(func):
         """
         Декоратор, который оборачивает функцию и добавляет дополнительное поведение.
         """
+
         def wrapper(*args):
             """
             Функция-обёртка, выполняющая дополнительные действия до и после вызова func.
             """
             result = func(*args)
             if not filename:
-                print("my_function error: тип ошибки. Inputs: (1, 2), {}")
-            else:
-                print("my_function ok")
+                my_file = open(filename, "w")
+                my_file.write("my_function ok")
+                my_file.close()
             return result
 
         return wrapper
 
     return my_decorator
 
+
+def predicate_log_positive(x, y):
+    """
+    Предикат проверяет на положительность
+    """
+    return x >= 0, y >= 0
+
+
+def predicate_log_negative(x, y):
+    """
+    Предикат проверяет на отрицательность
+    """
+    return x <= 0, y <= 0
+
+
 # Применение декоратора log
 @log(filename="mylog.txt")
 def my_function(x: int, y: int) -> int:
+    """
+    Декоратор суммирует параметры
+    """
     return x + y
+
 
 # Вызов функции
 my_function(1, 2)
+
+
+def test_log(capsys):
+    log(filename="mylog.txt")
+    captured = capsys.readouterr()
+    assert captured.out == "my_function error: тип ошибки. Inputs: (1, 2), {}"
+
+@pytest.fixture
+def test_log():
+    with pytest.raises(FileNotFoundError, match="Inputs: (1, 2)"):
+        my_function(1, 2)
