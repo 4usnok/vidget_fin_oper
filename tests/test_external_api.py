58345531f98e26dc0cf02515bb0db096db95310f
Nikolay Korkushko <Kakashonochka12@gmail.com>
2025-02-23 14:11:37 +0300

    Homework 12.1 fix.v.3

--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -5,6 +5,6 @@ from src.external_api import conv_value
 @patch('requests.get')
 def test_conv_value(mock_get):
     mock_get.return_value.json.return_value = {'base_code': 'USD', 'conversion_rates': {'RUB': 1}}
-    assert conv_value(1, 'USD') == 1
+    assert conv_value(1) == 1
     mock_get.assert_called_once_with(f"https://v6.exchangerate-api.com/v6/0dcfcddbc57fc16eaa3d6607/latest/USD")
 
