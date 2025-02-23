b9792eecd8bb21987fc7b0b36b7893c6ca3e7536
Nikolay Korkushko <Kakashonochka12@gmail.com>
2025-02-23 15:35:36 +0300

    Homework 12.1 fix.v.4

--- src/external_api.py
+++ src/external_api.py
@@ -8,18 +8,42 @@ load_dotenv()
 API_KEY = os.getenv("API_KEY")
 
 
+def trans_value(bank_oper):
+    code_currency = bank_oper["operationAmount"]["currency"]["code"]
+    value_sum = bank_oper["operationAmount"]["amount"]
+    if code_currency == "RUB":
+        return float(value_sum)
+    else:
+        result = conv_value(value_sum, code_currency)
+        return result
+
 # Создадим функцию, которая будет отправлять API-запрос
-def conv_value(amount):
+def conv_value(amount, code_value):
     """
     Принимает на вход название валют EUR или USD и конвертирует в RUB
     """
     # Создадим переменную, в которую поместим ссылку на API
-    url = f"https://v6.exchangerate-api.com/v6/{API_KEY}/latest/USD"
+    url = f"https://v6.exchangerate-api.com/v6/{API_KEY}/latest/{code_value}"
     response = requests.get(url)
-    if response.json()["base_code"] == "USD" or response.json()["base_code"] == "EUR":
-        result = response.json()["conversion_rates"]["RUB"]
-        return result * amount
+    result = response.json()["conversion_rates"]["RUB"]
+    return result * float(amount)
 
+
+new_div = {
+    "id": 441945886,
+    "state": "EXECUTED",
+    "date": "2019-08-26T10:50:58.294041",
+    "operationAmount": {
+      "amount": "31957.58",
+      "currency": {
+        "name": "руб.",
+        "code": "USD"
+      }
+    },
+    "description": "Перевод организации",
+    "from": "Maestro 1596837868705199",
+    "to": "Счет 64686473678894779589"
+  }
 
 # Распечатаем на экран результат конвертирования валют в рубли
-print(conv_value(3))
+print(trans_value(new_div))
--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -1,10 +1,38 @@
 from unittest.mock import patch
-from src.external_api import conv_value
+from src.external_api import trans_value
+
+
+
+
+def test_trans_value_in_rub():
+    new_div = {
+        "operationAmount": {
+            "amount": "31957.58",
+            "currency": {
+                "name": "руб.",
+                "code": "RUB"
+            }
+        }
+    }
+    assert trans_value(new_div) == 31957.58
 
 
 @patch('requests.get')
-def test_conv_value(mock_get):
-    mock_get.return_value.json.return_value = {'base_code': 'USD', 'conversion_rates': {'RUB': 1}}
-    assert conv_value(1) == 1
-    mock_get.assert_called_once_with(f"https://v6.exchangerate-api.com/v6/0dcfcddbc57fc16eaa3d6607/latest/USD")
+def test_trans_value_in_usd(mock_get):
+    new_div = {
+        "operationAmount": {
+            "amount": "1",
+            "currency": {
+                "name": "доллар",
+                "code": "USD"
+            }
+        }
+    }
+    mock_get.return_value.json.return_value = {"conversion_rates": {"RUB": 1}}
+    assert trans_value(new_div) == 1.00
+    #mock_get.assert_called_once_with(f"https://v6.exchangerate-api.com/v6/0dcfcddbc57fc16eaa3d6607/latest/USD")
 
+    # def test_conv_value(mock_get):
+    #     mock_get.return_value.json.return_value = {'base_code': 'USD', 'conversion_rates': {'RUB': 1}}
+    #     assert conv_value(1) == 1
+    #     mock_get.assert_called_once_with(f"https://v6.exchangerate-api.com/v6/0dcfcddbc57fc16eaa3d6607/latest/USD")
