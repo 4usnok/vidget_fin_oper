9445cd4f109f5be0c07c4579164fe827c2c26a85
Nikolay Korkushko <Kakashonochka12@gmail.com>
2025-02-23 14:04:30 +0300

    Homework 12.1 fix.v.3

--- src/external_api.py
+++ src/external_api.py
@@ -9,12 +9,12 @@ API_KEY = os.getenv("API_KEY")
 
 
 # Создадим функцию, которая будет отправлять API-запрос
-def conv_value(amount: float, value: str) -> int:
+def conv_value(amount):
     """
     Принимает на вход название валют EUR или USD и конвертирует в RUB
     """
     # Создадим переменную, в которую поместим ссылку на API
-    url = f"https://v6.exchangerate-api.com/v6/{API_KEY}/latest/{value}"
+    url = f"https://v6.exchangerate-api.com/v6/{API_KEY}/latest/USD"
     response = requests.get(url)
     if response.json()["base_code"] == "USD" or response.json()["base_code"] == "EUR":
         result = response.json()["conversion_rates"]["RUB"]
@@ -22,4 +22,4 @@ def conv_value(amount: float, value: str) -> int:
 
 
 # Распечатаем на экран результат конвертирования валют в рубли
-print(conv_value(2, "USD"))
+print(conv_value(3))
