6174021077caf0d36242b627212d4b8ad44b5552
Nikolay Korkushko <kakashonochka12@gmail.com>
2025-03-07 12:54:12 +0300

    homework_13_2 fixed

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import csv
 import json
-import pandas as pd
+from datetime import datetime
 from src.utils import func_json
 from src.csv_excel_work import csv_trans, excel_trans
 from src.processing import filter_by_state
 
 
-main_list = []
 def main():
     hello_input = input(f'Привет! Добро пожаловать в программу работы с банковскими транзакциями. '
                         f'\nВыберите необходимый пункт меню:'
@@ -46,46 +45,41 @@ def main():
 # Сортируем по дате:
 # Отсортируем json
     if hello_input == '1':
-        date_input = input('\nОтсортировать операции по дате? Да/Нет\n')
+        date_input = input('Отсортировать операции по дате? Да/Нет\n')
         if date_input.upper() == 'Да'.upper():
             sort_order = input('\nОтсортировать по возрастанию или по убыванию?\n')
             if sort_order.upper() == 'по возрастанию'.upper():
-                with open('data/file_work.json', encoding="utf-8") as json_file:
-                    result = json.load(json_file)
-                    sorted(result, key=lambda x: x["date"], reverse=False)
+                sorted(transactions_filter_status, key=lambda x: x["date"], reverse=False)
             elif sort_order.upper() == 'по убыванию'.upper():
-                with open('data/file_work.json', encoding="utf-8") as json_file:
-                    result = json.load(json_file)
-                    sorted(result, key=lambda x: x["date"], reverse=True)
+                sorted(transactions_filter_status, key=lambda x: x["date"], reverse=True)
 
 
 # Отсортируем csv
-    elif hello_input == '2':
-        with open(r'data/transactions.csv', 'r', encoding='utf-8') as csv_file:
-            sorter = list(csv.DictReader(csv_file, delimiter=';'))
-            date_input = input('\nОтсортировать операции по дате? Да/Нет\n')
-            if date_input.upper() == 'Да'.upper():
-                sort_order = input('\nОтсортировать по возрастанию или по убыванию?\n')
-                if sort_order.upper() == 'по возрастанию'.upper():
-                    result = sorted(sorter, key=lambda x: x["date"], reverse=False)
-                    for rise in result:
-                        print(rise)
-                elif sort_order.upper() == 'по убыванию'.upper():
-                    result = sorted(sorter, key=lambda x: x["date"], reverse=True)
-                    for decline in result:
-                        print(decline)
+#     elif hello_input == '2':
+#         with open(r'data/transactions.csv', 'r', encoding='utf-8') as csv_file:
+#             sorter = list(csv.DictReader(csv_file, delimiter=';'))
+#             date_input = input('\nОтсортировать операции по дате? Да/Нет\n')
+#             if date_input.upper() == 'Да'.upper():
+#                 sort_order = input('\nОтсортировать по возрастанию или по убыванию?\n')
+#                 if sort_order.upper() == 'по возрастанию'.upper():
+#                     result = sorted(sorter, key=lambda x: x["date"], reverse=False)
+#                     for rise in result:
+#                         print(rise)
+#                 elif sort_order.upper() == 'по убыванию'.upper():
+#                     result = sorted(sorter, key=lambda x: x["date"], reverse=True)
+#                     for decline in result:
+#                         print(decline)
 
 # Отсортируем excel
-    elif hello_input == '3':
-        workbook = pd.read_excel('data/transactions_excel.xlsx')
-        string_book = str(workbook['date'])
-        result = sorted(workbook, key=lambda x: x[string_book], reverse=True)
+#     elif hello_input == '3':
+#         workbook = pd.read_excel('data/transactions_excel.xlsx')
+#         string_book = str(workbook['date'])
+#         result = sorted(workbook, key=lambda x: x[string_book], reverse=True)
 
 # Напишем код для
     sort_trans_by_keyword = input('\nОтфильтровать список транзакций по определенному слову в описании? Да/Нет\n')
     if sort_trans_by_keyword.upper() == 'Да'.upper():
-        print('Распечатываю итоговый список транзакций...')
-        print(result)
+        print('\nРаспечатываю итоговый список транзакций...\n{transactions_filter_status}')
 
 
 
