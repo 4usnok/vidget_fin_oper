9bc055ebfb4b5d07d749679241359ef7e3ba63f9
Nikolay Korkushko <kakashonochka12@gmail.com>
2025-03-10 03:50:36 +0300

    homework_13_2 new_test

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,7 +1,18 @@
+import src.decorators
 from src.decorators import my_function
-
+import unittest
 
 def test_log(capsys):
     my_function(1, 2)
     captured = capsys.readouterr()
     assert captured.out == "my_function ok\n"
+
+
+class Testlog(unittest.TestCase):
+
+    def test_log(self):
+        self.assertEqual(src.decorators.my_function(1, 2), 3)
+
+
+if __name__ == '__main__':
+    unittest.main
