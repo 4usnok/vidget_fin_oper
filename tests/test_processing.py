c84651dc1843d718261e766309962d042bc40bbb
Nikolay Korkushko <kakashonochka12@gmail.com>
2025-03-10 05:59:04 +0300

    homework_13_2 new_test

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,5 +1,7 @@
+import src.processing
 from src.processing import filter_by_state, sort_by_date, filter_trans_by_des
 import pytest
+import unittest
 
 
 @pytest.mark.parametrize(
@@ -90,3 +92,29 @@ def test_sort_by_date(info, expected):
 )
 def test_filter_trans_by_des(info, expected):
     assert filter_trans_by_des(info, "Transaction") == expected
+
+
+class TestLog(unittest.TestCase):
+    def test_sort_by_date(self):
+        transactions = [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ]
+
+        # Вызов тестируемой функции
+        result = src.processing.sort_by_date(transactions)
+
+        # Ожидаемый результат
+        expected_result = [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            ]
+        # Проверка результата
+        self.assertEqual(result, expected_result)
+
+if __name__ == "__main__":
+    unittest.main()
